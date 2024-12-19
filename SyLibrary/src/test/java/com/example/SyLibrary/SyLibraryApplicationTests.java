996ae666c83822f62faa9be5a5048c91e4b806bc
dusdn <dusdn@192.168.219.104>
2024-12-19 14:51:51 +0900

    설정 커밋 완료

---
+++ SyLibrary/src/test/java/com/example/SyLibrary/SyLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SyLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
